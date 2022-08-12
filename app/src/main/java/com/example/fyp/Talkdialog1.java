6b9b5fdaa260497dac73df111754d62bc1ed63c2
Jason <jy149136@gmail.com>
2022-08-12 12:55:26 +0800

    update game story and modify dialog of final game

--- app/src/main/java/com/example/fyp/Talkdialog1.java
+++ app/src/main/java/com/example/fyp/Talkdialog1.java
@@ -22,30 +22,50 @@ public class Talkdialog1 extends AppCompatDialogFragment {
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        builder.setTitle("Little Elf")
-                .setMessage("I will tell you a story and then ask you a question. If you answer correctly, you will be given clues")
-                .setPositiveButton("ok", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        if(go_to_where.equals("game1")){
-                            Intent intent = new Intent(getActivity(),Game_Story.class);
-                            startActivity(intent);
-                        }
-                        if(go_to_where.equals("game2")){
-                            Intent intent = new Intent(getActivity(),Game2_story.class);
-                            startActivity(intent);
-                        }
-                        if(go_to_where.equals("game3")){
-                            Intent intent = new Intent(getActivity(),Game3_story.class);
-                            startActivity(intent);
-                        }
-                        if(go_to_where.equals("finalgame")){
-                            Intent intent = new Intent(getActivity(),Finalgame_story.class);
-                            startActivity(intent);
-                        }
 
-                    }
-                });
+        if(go_to_where.equals("finalgame")) {
+            builder.setTitle("Treasure chest")
+                    .setMessage("Unlock by answering questions correctly")
+                    .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            if(go_to_where.equals("finalgame")){
+                                Intent intent = new Intent(getActivity(),Finalgame_story.class);
+                                startActivity(intent);
+                            }
+
+                        }
+                    });
+        }
+        else{
+            builder.setTitle("Little Elf")
+                    .setMessage("I will tell you a story and then ask you a question. If you answer correctly, you will be given clues")
+                    .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            if(go_to_where.equals("game1")){
+                                Intent intent = new Intent(getActivity(),Game_Story.class);
+                                startActivity(intent);
+                            }
+                            if(go_to_where.equals("game2")){
+                                Intent intent = new Intent(getActivity(),Game2_story.class);
+                                startActivity(intent);
+                            }
+                            if(go_to_where.equals("game3")){
+                                Intent intent = new Intent(getActivity(),Game3_story.class);
+                                startActivity(intent);
+                            }
+                            if(go_to_where.equals("finalgame")){
+                                Intent intent = new Intent(getActivity(),Finalgame_story.class);
+                                startActivity(intent);
+                            }
+
+                        }
+                    });
+        }
+
+
+
 
         return builder.create();
     }
